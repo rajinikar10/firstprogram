7739fec21efad909a23601dc8379a8ce21ee988b
Rajinikar <rajinikar10@gmail.com>
2025-01-12 23:02:53 +0000

    sec

---
+++ src/firstprogram/Javaprog2.java
@@ -0,0 +1,11 @@
+package firstprogram;
+
+public class Javaprog2 {
+
+	public static void main(String[] args) {
+			// TODO Auto-generated method stub
+		System.out.println("Hello First program");
+
+	}
+
+}
